d8c570274652c05689d355c34cf38ee639860217
rexhent <dominichannah10@icloud.com>
2023-02-12 02:17:01 +1100

    first

---
+++ app/javascript/components/App.vue
@@ -0,0 +1,18 @@
+<template lang="">
+    <div>
+        <h1>Hello World!</h1>
+        <Counter />
+    </div>
+</template>
+<script>
+    import Counter from './Counter.vue'
+    export default {
+        components: {
+            Counter
+        }
+    
+    }
+</script>
+<style lang="">
+    
+</style>
---
+++ app/javascript/components/Counter.vue
@@ -0,0 +1,25 @@
+<template lang="">
+    <div>
+        <button @click="increment" id="counter-button">Increment</button>
+        <p>Count is: {{ count }}</p>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            count: 0
+        }
+    },
+    methods: {
+        increment() {
+            this.count++
+        }
+    }
+}
+</script>
+<style lang="css">
+    #counter-button {
+        
+    }
+</style>
